655f884c6e1753764725deb36307735a81c35868
niksolaz <solazzo.nicola@gmail.com>
2015-09-23 21:05:39 +0200

    Defined Tasks collection and loaded tasks from it

--- myapp/myapp.js
+++ myapp/myapp.js
@@ -1,10 +1,10 @@
-if (Meteor.isClient) {
-  
+Tasks = new Mongo.Collection("tasks");
+
+if(Meteor.isClient){
+  //this code only runs on the client
   Template.body.helpers({
-    tasks:[
-      { text: "This is task 1"},
-      { text: "This is task 2"},
-      { text: "This is task 3"}
-    ]
+    tasks:function(){
+      return Tasks.find({});
+    }
   });
 }
